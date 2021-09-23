9cad5e20d212a75d002d23b523f5790060288377
luisa16 <38504362+luisa16@users.noreply.github.com>
2021-09-23 17:28:09 +0200

    Add files via upload

--- test/ProgressBarTest.cpp
+++ test/ProgressBarTest.cpp
@@ -1,30 +1,13 @@
 #include "gtest/gtest.h"
 #include "../ProgressBar.h"
 
-TEST(ProgressBar, TestAttachDettach){
-    ResourceLoader rs;
-    auto pr=new ProgressBar(&rs);
-    auto i=std::find(rs.getObservers().begin(), rs.getObservers().end(), pr);
-    ASSERT_NE(i, rs.getObservers().end());
-    //pr->detach();
-    delete pr;
-    auto j=std::find(rs.getObservers().begin(), rs.getObservers().end(), pr);
-    ASSERT_EQ(j, rs.getObservers().end());
-}
 
 TEST(ProgressBar, TestUpdate){
     ResourceLoader rs;
     auto pr=new ProgressBar(&rs);
     std::string p="/home/luisa/Documenti/prova2.txt";
     rs.notifyObservers();
-    ASSERT_TRUE(rs.isNotified());
     ASSERT_TRUE(pr->isUpdated());
 }
 
-/*TEST(ProgressBar, TestUpdate){
-    ResourceLoader rs;
-    auto pr=new ProgressBar(&rs);
-    rs.ReadFile("/home/luisa/Documenti/prova2.txt");
-    ASSERT_TRUE(pr->isUpdated());
-}*/
 
--- test/ResourceLoaderTest.cpp
+++ test/ResourceLoaderTest.cpp
@@ -4,24 +4,12 @@
 
 
 TEST(ResourceLoader, TestConstructor) {
-
     ResourceLoader rs;
-
     ASSERT_EQ(0, rs.getFileSize());
-    ASSERT_FALSE(rs.getSelected());
     ASSERT_EQ("", rs.getFileName());
     ASSERT_FALSE(rs.isNotified());
 }
 
-TEST(ResourceLoader, TestRegisterUnregister) {
-    ResourceLoader rs;
-    Observer *pr = new ProgressBar(&rs);
-    //rs.registerObserver(pr);
-    auto i = std::find(rs.getObservers().begin(), rs.getObservers().end(), pr);
-    ASSERT_EQ(i, rs.getObservers().begin());
-    rs.removeObserver(pr);
-    ASSERT_TRUE(rs.getObservers().empty());
-}
 
 
 TEST(ResourceLoader, TestReadFile) {
@@ -40,7 +28,7 @@ TEST(ResourceLoader, TestNotify){
     ResourceLoader rs;
     auto pr=new ProgressBar(&rs);
     std::string p="/home/luisa/Documenti/prova2.txt";
-    rs.notifyObservers();
+    rs.ReadFile(p);
     ASSERT_TRUE(rs.isNotified());
 }
 
